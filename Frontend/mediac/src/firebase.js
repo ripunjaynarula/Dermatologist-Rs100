3b4e83690e7aabdb753e2502c64c4e0442c11859
Shiv_10 <shivanshsharma2012@gmail.com>
2021-02-06 22:17:12 +0530

    fix: fix firebase settings

--- Frontend/mediac/src/firebase.js
+++ Frontend/mediac/src/firebase.js
@@ -2,13 +2,12 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyCKs8TgGuvwMec2ebnz6-zhYZUVsimkjpg",
+    authDomain: "mediac-fda09.firebaseapp.com",
+    projectId: "mediac-fda09",
+    storageBucket: "mediac-fda09.appspot.com",
+    messagingSenderId: "1068406252700",
+    appId: "1:1068406252700:web:a8abd468964d35cce97959"
 })
 
 export const auth = app.auth()

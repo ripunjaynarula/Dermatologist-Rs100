3f355f0185228d570a1521ef9acaba8cfc448572
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-02-08 17:06:12 +0530

    console error fixed

--- Frontend/mediac/src/components/Navbar.js
+++ Frontend/mediac/src/components/Navbar.js
@@ -14,18 +14,15 @@ const NavBar = () => {
   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
     <ReactBootStrap.Nav className="mr-auto"> 
-    <Link to="/about">
+    
     <ReactBootStrap.Nav.Link className="navlink" href="/about">About</ReactBootStrap.Nav.Link>
-    </Link>
-    <Link to="/Blog">
+    
     <ReactBootStrap.Nav.Link className="navlink" href="/blog">Blog</ReactBootStrap.Nav.Link>
-    </Link>
-    <Link to="/FAQ">
+    
     <ReactBootStrap.Nav.Link  className="navlink" href="/FAQ">FAQs</ReactBootStrap.Nav.Link>
-    </Link>
-    <Link to="/Contact">
+    
     <ReactBootStrap.Nav.Link className="navlink"  href="/contact">Contact</ReactBootStrap.Nav.Link>
-    </Link>
+    
      {/*<ReactBootStrap.NavDropdown title="YEET" id="collasible-nav-dropdown">
         <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
@@ -35,14 +32,14 @@ const NavBar = () => {
     </ReactBootStrap.NavDropdown>*/}
     </ReactBootStrap.Nav>
     <ReactBootStrap.Nav>
-    <Link to="/login">
+    
     <ReactBootStrap.Nav.Link href="/login">Login</ReactBootStrap.Nav.Link>
-    </Link>
-    <Link to="/signup">
+    
+    
     <ReactBootStrap.Nav.Link eventKey={2} href="/Signup">
         Signup
       </ReactBootStrap.Nav.Link>
-    </Link>
+    
     </ReactBootStrap.Nav>
   </ReactBootStrap.Navbar.Collapse>
 </ReactBootStrap.Navbar>

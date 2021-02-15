e0a09578cb904435613bc041f356c9054e4aa58f
Shiv_10 <shivanshsharma2012@gmail.com>
2021-02-16 03:13:51 +0530

    fix: fix display on consultancy card

--- Frontend/mediac/src/components/ConsultancyCard.js
+++ Frontend/mediac/src/components/ConsultancyCard.js
@@ -11,24 +11,33 @@ export default function ConsultancyCard(props) {
 
     
 
-return(
+return (
 <>
 
 <div className="card">
-  
   <div className="container" id="cardcontainer">
     <h2><b>Name: {props.name}</b></h2>
-    <hr></hr>
-    <p><b>Doctor's Name:</b> Dr.{props.doctorname}</p>
-    <p style={{ align: 'right', display:'relative'}}><b>Start Date:</b> {props.startdate}</p>
-    <p><b>Last Consultation Date: </b>{props.lastconsult}</p>
-    <p><b>Age:</b> {props.age} years </p>
-    <p><b>Height: </b>{props.height} cm</p>
-    <p><b>Weight:</b> {props.weight} kg</p>
-    <div id="cardbtn">
-    <p href="#" className="bookbtn">Continue Consultation</p>
-    <p href="#" className="endbtn">End Consultation</p>
+    <hr />
+      <div style={{position: "relative"}}>
+        <div style={{float: "left"}}>
+          <p><b>Doctor's Name:</b> Dr.{props.doctorname}</p>
+          <p style={{ align: 'right', display:'relative'}}><b>Start Date:</b> {props.startdate}</p>
+          <p>
+            <b>Last appointment Date: </b>{props.lastconsult}
+          </p>
+        </div>
+        <div style={{float: "right"}}>
+          <p><b>Age:</b> {props.age} years</p>
+          <p><b>Height: </b>{props.height}</p>
+          <p><b>Weight:</b> {props.weight}</p>
+        </div>
     </div>
+  </div>
+  <div >
+      <div id="cardbtn" style={{padding: "2px 16px"}}>
+        <p href="#" className="bookbtn">Continue Consultation</p>
+        <p href="#" className="endbtn">End Consultation</p>
+      </div>
   </div>
 </div>
     
--- Frontend/mediac/src/components/Dashboard.js
+++ Frontend/mediac/src/components/Dashboard.js
@@ -54,7 +54,7 @@ export default function Dashboard() {
   return (
     <>
     <div id="consultancycard">
-      <ConsultancyCard  name="ok" doctorname="saif" startdate="69-69-6969" lastconsult="09-12-2019" age="20" height="180 cm" weight="69 kg"/>     
+      <ConsultancyCard  name="Shivansh Sharma" doctorname="Sehgal" startdate="10-01-2021" lastconsult="15-02-2021" age="20" height="180 cm" weight="69 kg"/>     
     
     </div>
     </>

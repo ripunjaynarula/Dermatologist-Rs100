9ad92e8cd510f7b4836b644823fa3249c41c8ec2
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-02-16 12:17:31 +0530

    card css fixed

--- Frontend/mediac/src/components/ConsultancyCard.js
+++ Frontend/mediac/src/components/ConsultancyCard.js
@@ -19,14 +19,14 @@ return (
     <h2><b>Name: {props.name}</b></h2>
     <hr />
       <div style={{position: "relative"}}>
-        <div style={{float: "left"}}>
+        <div id="cardprimary">
           <p><b>Doctor's Name:</b> Dr.{props.doctorname}</p>
           <p style={{ align: 'right', display:'relative'}}><b>Start Date:</b> {props.startdate}</p>
           <p>
             <b>Last appointment Date: </b>{props.lastconsult}
           </p>
         </div>
-        <div style={{float: "right"}}>
+        <div id= "cardsecondary" >
           <p><b>Age:</b> {props.age} years</p>
           <p><b>Height: </b>{props.height}</p>
           <p><b>Weight:</b> {props.weight}</p>

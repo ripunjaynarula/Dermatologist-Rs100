fe1a6e69ac91dda5f546c1a7a21041020c4430cc
Shiv_10 <shivanshsharma2012@gmail.com>
2021-02-18 01:37:28 +0530

    feat: add fetch for active consultation

--- Backend/src/routes/getActiveConsultation.ts
+++ Backend/src/routes/getActiveConsultation.ts
@@ -3,11 +3,10 @@ import consultations from '../models/consultation'
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    console.log(req.body.email);
+router.post('/', async (req, res) => {
     const consultation: any = await consultations.findOne({$and: [ {patientEmail: req.body.email}, {active: true} ]});
     if (consultation){
-        return res.send({status: true, startDate: consultation.startDate, title: consultation.title, age: consultation.age, weight: consultation.weight, height: consultation.height});
+        return res.send({status: true, startDate: consultation.startDate, title: consultation.title, age: consultation.age, weight: consultation.weight, height: consultation.height, docMail: consultation.doctorEmail});
     }
     return res.send({status: false})
 });
--- Frontend/mediac/src/components/ConsultancyCard.js
+++ Frontend/mediac/src/components/ConsultancyCard.js
@@ -1,47 +1,81 @@
 import React, { useState, useEffect } from "react";
-import { Card, Button, Alert } from "react-bootstrap"
+// import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
-import {  useHistory } from "react-router-dom"
-import Accordion from "./Accordion";
-import  "./styles.css";
+// import {  useHistory } from "react-router-dom"
+// import Accordion from "./Accordion";
+// import  "./styles.css";
 
 
 
 export default function ConsultancyCard(props) {
 
+  const [title, setTitle] = useState('');
+  const [age, setAge] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [docMail, setDocMail] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [active, setActive] = useState(false);
+
+
+
+  const { currentUser, logout } = useAuth()
+  useEffect( () => {
+    async function fetchData() {
+      if (currentUser) {
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ email: currentUser.email })
+          };
+        let res = await fetch('http://localhost:5000/getActiveConsultation', requestOptions);
+        res = await res.text();
+        res = JSON.parse(res)
+        console.log(res)
+        if (res['status']) {
+          setTitle(res['title']);
+          setAge(res['age']);
+          setHeight(res['height']);
+          setWeight(res['weight']);
+          setDocMail(res['docMail']);
+          setStartDate(res['startDate'])
+          setActive(true);
+        }
+      }
+    }
+    fetchData();
+  }, [currentUser])
     
 
-return (
-<>
-
-<div className="card">
-  <div className="container" id="cardcontainer">
-    <h2><b>Name: {props.name}</b></h2>
-    <hr />
-      <div style={{position: "relative"}}>
-        <div id="cardprimary">
-          <p><b>Doctor's Name:</b> Dr. {props.doctorname}</p>
-          <p style={{ align: 'right', display:'relative'}}><b>Start Date:</b> {props.startdate}</p>
-          <p>
-            <b>Last appointment Date: </b>{props.lastconsult}
-          </p>
+  return (
+  <>
+    { active?<div className="card">
+      <div className="container" id="cardcontainer">
+        <h2><b>Title: {title}</b></h2>
+        <hr />
+          <div style={{position: "relative"}}>
+            <div id="cardprimary">
+              <p><b>Doctor's Email:</b> {docMail}</p>
+              <p style={{ align: 'right', display:'relative'}}><b>Start Date:</b> {startDate}</p>
+              <p>
+                <b>Active Status: </b> Active
+              </p>
+            </div>
+            <div id= "cardsecondary" >
+              <p><b>Age:</b> {age} years</p>
+              <p><b>Height: </b>{height} cm</p>
+              <p><b>Weight:</b> {weight} Kg</p>
+            </div>
         </div>
-        <div id= "cardsecondary" >
-          <p><b>Age:</b> {props.age} years</p>
-          <p><b>Height: </b>{props.height}</p>
-          <p><b>Weight:</b> {props.weight}</p>
-        </div>
-    </div>
-  </div>
-  <div >
-      <div id="cardbtn" style={{padding: "2px 16px"}}>
-        <p href="#" className="bookbtn">Continue Consultation</p>
-        <p href="#" className="endbtn">End Consultation</p>
       </div>
-  </div>
-</div>
-    
-
-</>
+      <div >
+          <div id="cardbtn" style={{padding: "2px 16px"}}>
+            <p href="#" className="bookbtn">Continue Consultation</p>
+            <p href="#" className="endbtn">End Consultation</p>
+          </div>
+      </div>
+    </div>:<p>No active consultations</p>
+  }
+  </>
 
 )}

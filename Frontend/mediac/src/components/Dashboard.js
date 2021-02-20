ff40924122cacaeb512af15604a8d2d9e74c0f38
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-02-20 23:34:24 +0530

    btn error fix

--- Frontend/mediac/src/components/Dashboard.js
+++ Frontend/mediac/src/components/Dashboard.js
@@ -58,7 +58,7 @@ export default function Dashboard() {
     <div id="consultancycard">
       <ConsultancyCard  name="Shivansh Sharma" doctorname="Sehgal" startdate="10-01-2021" lastconsult="15-02-2021" age="20" height="180 cm" weight="69 kg"/>     
     </div>
-    <a href="/ConsultationForm" id="newconbtn"><img id="ellipsebtn" src={ellipse}/> New Consultation</a>
+    <a href="/Choice" id="newconbtn"><img id="ellipsebtn" src={ellipse}/> New Consultation</a>
     </>
 
   )

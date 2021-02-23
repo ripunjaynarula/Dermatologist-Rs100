27067072f2187e2b38f15a8946d34eb3dfd54136
Shiv_10 <shivanshsharma2012@gmail.com>
2021-02-23 22:59:24 +0530

    feat: added route for getting profiles

---
+++ Backend/src/routes/getProfiles.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import patients from '../models/patients'
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const patient: any = await patients.findOne({uid: req.body.uid});
+    if (patient) {
+        let profiles = patient.profiles;
+        console.log(profiles);
+        return res.send({status: 'success', profiles: profiles});
+    }
+    res.send({status: 'no_account_found'});
+});
+
+export default router;

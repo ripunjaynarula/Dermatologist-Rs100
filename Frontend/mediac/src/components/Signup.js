991395b1faf8d8ce7bee4158875a649aebf2eb84
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-03-03 17:42:33 +0530

    container added

--- Frontend/mediac/src/components/Signup.js
+++ Frontend/mediac/src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react"
-import { Form, Card, Alert, Button } from "react-bootstrap"
+import { Form, Card, Alert, Button, Container } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 import { auth } from '../firebase'

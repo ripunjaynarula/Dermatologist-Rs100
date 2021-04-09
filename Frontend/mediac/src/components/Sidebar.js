ab3f9566127fdd5e31ce882fe6e3dbc8e557119a
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-04-10 04:31:16 +0530

    chat:wip

--- Frontend/mediac/src/components/Sidebar.js
+++ Frontend/mediac/src/components/Sidebar.js
@@ -1,46 +1,42 @@
-import React, { useRef, useState, useEffect } from "react"
-import { Tab, Nav, Button, Modal } from 'react-bootstrap'
-import Conversation from './Conversation'
-function Sidebar() {
+import React, { useRef, useState, useEffect } from "react";
+import { Tab, Nav, Button, Modal, Form } from "react-bootstrap";
+import Conversation from "./Conversation";
+import  "./styles.css";
 
-    const [modalOpen, setModalOpen] = useState(false)
+function Sidebar() {
+  const [modalOpen, setModalOpen] = useState(false);
   function closeModal() {
-    setModalOpen(false)
+    setModalOpen(false);
   }
 
-    return (
-        <div>
-            <div style={{ width: '250px' }} className="d-flex flex-column">
-      <Tab.Container>
-        <Nav variant="tabs" className="justify-content-center">
-          <Nav.Item>
-            <Nav.Item >Conversations</Nav.Item>
-          </Nav.Item>
-          
-        </Nav>
-        <Tab.Content className="border-right overflow-auto flex-grow-1">
-          <Tab.Pane >
-            <Conversation/>
-          </Tab.Pane>
-          
-        </Tab.Content>
-        {/* <div className="p-2 border-top border-right small">
-          Your Id: <span className="text-muted"></span>
-        </div> */}
-        {/* <Button onClick={() => setModalOpen(true)} className="rounded-0">
-          New {conversationsOpen ? 'Conversation' : 'Contact'}
-        </Button> */}
-      </Tab.Container>
+  return (
+    <div>
+      <div style={{ width: "250px" }} className="d-flex flex-column">
+        <Tab.Container>
+          <div className="justify-content-center">
+            <Form.Group style={{ paddingTop: 14 }}>
+              <Form.Control
+                type="text"
+                className="w-100"
+                placeholder="Search"
+                required
+              />
+            </Form.Group>
+          </div>
+          <Nav variant="tabs" className="justify-content-center">
+            
+              <Nav.Item><b>Conversations</b></Nav.Item>
+            
+          </Nav>
+          <Tab.Content className=" overflow-auto flex-grow-1">
+            <Conversation />
+          </Tab.Content>
+        
+        </Tab.Container>
 
-      {/* <Modal show={modalOpen} onHide={closeModal}>
-        {conversationsOpen ?
-          <NewConversationModal closeModal={closeModal} /> :
-          <NewContactModal closeModal={closeModal} />
-        }
-      </Modal> */}
+      </div>
     </div>
-        </div>
-    )
+  );
 }
 
-export default Sidebar
+export default Sidebar;

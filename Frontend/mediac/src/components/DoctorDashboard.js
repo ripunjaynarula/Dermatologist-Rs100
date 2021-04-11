07e70a983c1664ea5ef120f1181be8b30f567ea5
Shiv10 <shivanshsharma2012@gmail.com>
2021-04-11 19:07:44 +0530

    fix: fix doc dashboard

--- Frontend/mediac/src/components/DoctorDashboard.js
+++ Frontend/mediac/src/components/DoctorDashboard.js
@@ -1,77 +1,95 @@
-
-import { Container, Card, CardBody,Row, Col } from "reactstrap"
+import { Container, Card, CardBody, Row, Col } from "reactstrap";
 import React, { useState, useRef, useEffect, useContext } from "react";
-import { useAuth } from "../contexts/AuthContext"
-import {  useHistory } from "react-router-dom"
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
-import Header from './Header';
-import DocNav from './DocNav';
-import Navbar from "./Navbar"
-import docimg from './img/doc.jpeg';
-import  "./styles.css";
-import  "./docdash.css";
+import { useAuth } from "../contexts/AuthContext";
+import { useHistory } from "react-router-dom";
+import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import Header from "./Header";
+import DocNav from "./DocNav";
+import Navbar from "./Navbar";
+import docimg from "./img/doc.jpeg";
+import "./styles.css";
+import "./docdash.css";
 
-import usersvg from './img/user.svg';
+import usersvg from "./img/user.svg";
 
-import {DocMailContext} from './App';
+import { DocMailContext } from "./App";
 
 export default function DoctorDashboard() {
-  
   const [show, setShow] = useState(false);
-  const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const dbinfo = useRef()
-  const history = useHistory()
-  const quest = useRef()
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const dbinfo = useRef();
+  const history = useHistory();
+  const quest = useRef();
 
-
-  useEffect(() =>{
- 
- 
-    async function checkLogin(){
-                const token = await  currentUser.getIdToken(true);
+  useEffect(() => {
+    async function checkLogin() {
+      const token = await currentUser.getIdToken(true);
 
       const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({token : token})
-      }
+        method: "POST",
+        headers: { "Content-Type": "application/json", token: token },
+        body: JSON.stringify({ email: currentUser.email }),
+      };
 
-      let res = await fetch('http://localhost:5000/getDocDetails', requestOptions);
+      let res = await fetch(
+        "http://localhost:5000/getDocDetails",
+        requestOptions
+      );
       res = await res.text();
-      res = JSON.parse(res)
+      res = JSON.parse(res);
       console.log(res);
-      if(!res['status']){
-        history.push('/login');
-      }
-      else{
- 
+      if (!res["status"]) {
+        history.push("/login");
+      } else {
       }
     }
     checkLogin();
-  }, [])
+  }, []);
 
   return (
     <>
-    <Header/>
-<br/>
-    <Container id="doc" className="d-flex align-items-center justify-content-center">
-    <Card id="doccard">
-                <CardBody >
-                <img id="docimg" src={docimg}/>    <br/><br/>
-
-                <p id="posttitle">Post Title</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec eratLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat.</p>
-                </CardBody>
-                <Row style= {{paddingTop :"22px", flexDirection: 'row', justifyContent: 'flex-end',paddingRight: "34px", paddingBottom: "4px" }}>
-
-                </Row>
-                </Card>
-                
-    </Container> <br/> <br/>
-    
-    {/* <Container  className="d-flex align-items-center justify-content-center">
+      <Header />
+      <br />
+      <Container
+        id="doc"
+        className="d-flex align-items-center justify-content-center"
+      >
+        <Card id="doccard">
+          <CardBody>
+            <img id="docimg" src={docimg} /> <br />
+            <br />
+            <p id="posttitle">Post Title</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              maximus, nulla ut commodo sagittis, sapien dui mattis dui, non
+              pulvinar lorem felis nec erat.Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+              sagittis, sapien dui mattis dui, non pulvinar lorem felis nec
+              erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              maximus, nulla ut commodo sagittis, sapien dui mattis dui, non
+              pulvinar lorem felis nec eratLorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+              sagittis, sapien dui mattis dui, non pulvinar lorem felis nec
+              erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              maximus, nulla ut commodo sagittis, sapien dui mattis dui, non
+              pulvinar lorem felis nec erat.
+            </p>
+          </CardBody>
+          <Row
+            style={{
+              paddingTop: "22px",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              paddingRight: "34px",
+              paddingBottom: "4px",
+            }}
+          ></Row>
+        </Card>
+      </Container>{" "}
+      <br /> <br />
+      {/* <Container  className="d-flex align-items-center justify-content-center">
     <div class="card mb-3" >
   <div class="row no-gutters">
     <div class="col-md-4">
@@ -87,8 +105,8 @@ export default function DoctorDashboard() {
   </div>
 </div>
 </Container> */}
-    <br/><br/>
+      <br />
+      <br />
     </>
-
-  )
+  );
 }

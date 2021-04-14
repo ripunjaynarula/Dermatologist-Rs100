be9a41e04c1a752b6a32b7b2b33b712a5f4cb3b9
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-04-14 15:01:31 +0530

    icon added archive

--- Frontend/mediac/src/components/OpenConversation.js
+++ Frontend/mediac/src/components/OpenConversation.js
@@ -1,15 +1,24 @@
-import React, { useEffect, useState, useContext, useRef, useCallback } from "react";
-import ScrollToBottom, { useScrollToBottom, useSticky } from 'react-scroll-to-bottom';
-import usersvg from './img/user.svg';
+import React, {
+  useEffect,
+  useState,
+  useContext,
+  useRef,
+  useCallback,
+} from "react";
+import ScrollToBottom, {
+  useScrollToBottom,
+  useSticky,
+} from "react-scroll-to-bottom";
+import usersvg from "./img/user.svg";
 
 import {
   BrowserView,
   MobileView,
   isBrowser,
-  isMobile
-} 
-from "react-device-detect";
-import { AiOutlineArrowLeft } from 'react-icons/ai';
+  isMobile,
+} from "react-device-detect";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { FiArchive } from "react-icons/fi";
 
 import { Form, InputGroup, Button } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
@@ -19,103 +28,111 @@ import "./styles.css";
 import app from "../firebase";
 import { CurrentChatContext } from "./App";
 import loadimg from "./img/loading.webp";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 function OpenConversation() {
-  
   const messageRef = useRef();
   const [chatData, setChatData] = useState({});
   const { currentUser } = useAuth();
   const history = useHistory();
   const [socket, setSocket] = useState();
-  const [prevChat, setPrevChat] = useState('');
+  const [prevChat, setPrevChat] = useState("");
   const [currentChat, setCurrentChat] = useContext(CurrentChatContext);
-  const messageEndRef = useRef(null)
- 
+  const messageEndRef = useRef(null);
 
   function handleSubmit(e) {
-    
     e.preventDefault();
     var time = new Date();
     var options = {
       year: "numeric",
       month: "2-digit",
-      day: "numeric"
-  };
+      day: "numeric",
+    };
     let msgData = {
       date: time.toLocaleDateString("en", options),
       from: currentUser.email,
-      time: time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+      time: time.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      }),
       text: messageRef.current.value,
-    }
-    if(socket){
-      socket.emit('send', msgData);
+    };
+    if (socket) {
+      socket.emit("send", msgData);
     }
     chatData["messages"].push(msgData);
     //append child
-    const chatDiv = document.getElementById('chatMessages');
-    const messageDiv = document.createElement('div');
-    messageDiv.className = 'align-items-end my-1 align-self-end flex-column d-flex mine';
-    const textDiv = document.createElement('div');
-    textDiv.className = 'text-white bg-primary py-1 px-2 rounded';
+    const chatDiv = document.getElementById("chatMessages");
+    const messageDiv = document.createElement("div");
+    messageDiv.className =
+      "align-items-end my-1 align-self-end flex-column d-flex mine";
+    const textDiv = document.createElement("div");
+    textDiv.className = "text-white bg-primary py-1 px-2 rounded";
     textDiv.textContent = messageRef.current.value;
     messageDiv.appendChild(textDiv);
-    const timeDiv=document.createElement('div');
-    timeDiv.className = 'text-muted small date text-right';
-    timeDiv.textContent=time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    const timeDiv = document.createElement("div");
+    timeDiv.className = "text-muted small date text-right";
+    timeDiv.textContent = time.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
     messageDiv.appendChild(timeDiv);
     chatDiv.appendChild(messageDiv);
     messageRef.current.value = "";
-    messageEndRef.current.scrollIntoView({behavior: "smooth"});
+    messageEndRef.current.scrollIntoView({ behavior: "smooth" });
   }
-  const handleKeypress = e => {
+  const handleKeypress = (e) => {
     e.preventDefault();
-      if (e.keyCode === 13) {
-    handleSubmit();
-  }
-  
-  
-};
+    if (e.keyCode === 13) {
+      handleSubmit();
+    }
+  };
   const handleNewMessage = useCallback((msgData) => {
-    if(chatData['messages']){
+    if (chatData["messages"]) {
       chatData["messages"].push(msgData);
     }
     //append child
     var time = new Date();
-    const chatDiv = document.getElementById('chatMessages');
-    const messageDiv = document.createElement('div');
-    messageDiv.className = 'align-items-start my-1 flex-column d-flex their';
-    const textDiv = document.createElement('div');
-    textDiv.className = 'bg-light py-1 px-2 rounded';
-    textDiv.textContent = msgData['text'];
+    const chatDiv = document.getElementById("chatMessages");
+    const messageDiv = document.createElement("div");
+    messageDiv.className = "align-items-start my-1 flex-column d-flex their";
+    const textDiv = document.createElement("div");
+    textDiv.className = "bg-light py-1 px-2 rounded";
+    textDiv.textContent = msgData["text"];
     messageDiv.appendChild(textDiv);
-    const timeDiv=document.createElement('div');
-    timeDiv.className = 'text-muted small date';
-    timeDiv.textContent=time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    const timeDiv = document.createElement("div");
+    timeDiv.className = "text-muted small date";
+    timeDiv.textContent = time.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
     messageDiv.appendChild(timeDiv);
     chatDiv.appendChild(messageDiv);
     messageRef.current.value = "";
-    messageEndRef.current.scrollIntoView({behavior: "smooth"});
+    messageEndRef.current.scrollIntoView({ behavior: "smooth" });
   }, []);
 
-  const handleBackButton =() => {
-    console.log('Clicked');
+  const handleBackButton = () => {
+    console.log("Clicked");
     setCurrentChat("");
-  }
-  const handleArchiveButton =() => {
+  };
+  const handleArchiveButton = async () => {
     const token = await app.auth().currentUser.getIdToken(true);
     const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json", token: token },
-        body: JSON.stringify({ chatId: currentChat }),
+      method: "POST",
+      headers: { "Content-Type": "application/json", token: token },
+      body: JSON.stringify({ chatId: currentChat }),
     };
     let res = await fetch(
-        "http://localhost:5000/toggleArchive",
-        requestOptions
+      "http://localhost:5000/toggleArchive",
+      requestOptions
     );
     res = await res.text();
     res = JSON.parse(res);
-  }
+  };
 
   useEffect(() => {
     async function getChats() {
@@ -127,7 +144,9 @@ function OpenConversation() {
         return;
       }
       setPrevChat(currentChat);
-      const newSocket = io('http://localhost:5000/', { query: { currentChat } });
+      const newSocket = io("http://localhost:5000/", {
+        query: { currentChat },
+      });
       setSocket(newSocket);
       const token = await app.auth().currentUser.getIdToken(true);
       const requestOptions = {
@@ -144,47 +163,54 @@ function OpenConversation() {
       console.log(res);
       setChatData(res["chats"]);
       console.log(chatData);
-      messageEndRef.current.scrollIntoView({behavior: "smooth"});
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
       return () => newSocket.close();
     }
     getChats();
-    if(!socket) return;
-    socket.on('new-message', handleNewMessage);
-    return () => socket.off('new-message');
+    if (!socket) return;
+    socket.on("new-message", handleNewMessage);
+    return () => socket.off("new-message");
   }, [currentChat, socket, handleNewMessage]);
 
   if (chatData["messages"]) {
     return (
       <>
-      
         <div className="contacthead">
-          <div style={{float: "left"}}>
-          {isMobile?(<>
-              <Button onClick={handleBackButton}>
-                <AiOutlineArrowLeft/>
-              </Button>&nbsp;&nbsp;</>):(<></>)}
-              <img id="userimg" style={{width:"13%"}} src={usersvg}/>&nbsp;&nbsp;
+          <div style={{ float: "left" }}>
+            {isMobile ? (
+              <>
+                <Button onClick={handleBackButton}>
+                  <AiOutlineArrowLeft />
+                </Button>
+                &nbsp;&nbsp;
+              </>
+            ) : (
+              <></>
+            )}
+            <img id="userimg" style={{ width: "13%" }} src={usersvg} />
+            &nbsp;&nbsp;
             {chatData["doctorEmail"] === currentUser.email
               ? chatData["patientUsername"]
               : chatData["doctorUsername"]}
           </div>
-         
-          
+
           <hr />
-          <div style={{float: "right"}}><Button id="cancelbtn" onClick={handleArchiveButton}>Archive</Button></div>
+          <div style={{ float: "right" }}>
+            <Button id="cancelbtn" onClick={handleArchiveButton}>
+              <FiArchive/>
+            </Button>
+          </div>
         </div>
         <div
           className="d-flex flex-column flex-grow-1 chatbg "
           style={{ marginTop: "-1.7%" }}
           id="chatbox"
         >
-          <div className="flex-grow-1 overflow-auto" id="chatMessages" >
-
+          <div className="flex-grow-1 overflow-auto" id="chatMessages">
             <br />
 
             {chatData["messages"].map((message) => (
               <>
-              
                 <div
                   className={`my-1 d-flex flex-column ${
                     currentUser.email === message["from"]
@@ -213,25 +239,29 @@ function OpenConversation() {
             ))}
             <div ref={messageEndRef}></div>
           </div>
-          
-          <Form onSubmit={handleSubmit} autocomplete="off" >
+
+          <Form onSubmit={handleSubmit} autocomplete="off">
             <Form.Group className="m-2">
               <InputGroup id="bottommsg" style={{ height: "40px" }}>
-                <Form.Control 
+                <Form.Control
                   id="sendmsg"
                   as="textarea"
                   ref={messageRef}
                   required
                   placeholder="Type your message here..."
-                  onKeyPress={event =>{
-                    if(event.key=="Enter"){
+                  onKeyPress={(event) => {
+                    if (event.key == "Enter") {
                       handleSubmit(event);
                     }
-                  }
-                }
-
-                style={{ height: "40px", resize: "none", borderRadius:"4px", display: "flex", fontSize:"14px",paddingTop: "8px" }}
-
+                  }}
+                  style={{
+                    height: "40px",
+                    resize: "none",
+                    borderRadius: "4px",
+                    display: "flex",
+                    fontSize: "14px",
+                    paddingTop: "8px",
+                  }}
                 />
                 <InputGroup.Append>
                   <Button type="submit" onKeyPress={handleKeypress}>

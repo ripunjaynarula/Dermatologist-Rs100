9f5bd72135a78cb990b251850b89110e0240c07f
Shiv10 <shivanshsharma2012@gmail.com>
2021-04-15 23:56:28 +0530

    fix: code formatting

--- Frontend/mediac/src/components/ForgotPassword.js
+++ Frontend/mediac/src/components/ForgotPassword.js
@@ -1,76 +1,70 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Container,Card, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
-import { Link } from "react-router-dom"
-import {CardMain} from "../css/Card";
-import {Texts} from "../css/Texts";
-import Navbar from "./Navbar"
+import React, { useRef, useState } from "react";
+import { Form, Button, Container, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
+import { CardMain } from "../css/Card";
+import { Texts } from "../css/Texts";
+import Navbar from "./Navbar";
 export default function ForgotPassword() {
-  const emailRef = useRef()
-  const { resetPassword } = useAuth()
-  const [error, setError] = useState("")
-  const [message, setMessage] = useState("")
-  const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const { resetPassword } = useAuth();
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      setMessage("")
-      setError("")
-      setLoading(true)
-   var res =   await resetPassword(emailRef.current.value)
-   console.log(res)
-      setMessage("Check your inbox for further instructions")
+      setMessage("");
+      setError("");
+      setLoading(true);
+      var res = await resetPassword(emailRef.current.value);
+      console.log(res);
+      setMessage("Check your inbox for further instructions");
     } catch {
-      setError("Failed to reset password")
+      setError("Failed to reset password");
     }
 
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
     <>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "90vh" }}
+      >
+        <Card style={({ CardMain }, { maxWidth: "400px" })}>
+          <Card.Title></Card.Title>
+          <Card.Body>
+            <h4 className="text-center mb-4" style={Texts.Heading}>
+              Password Reset
+            </h4>
 
-   <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "90vh" }}>
-      
-      <Card style={{CardMain},{ maxWidth: "400px" }}  >
-<Card.Title>
+            <hr></hr>
+            {error && <Alert variant="danger">{error}</Alert>}
+            {message && <Alert variant="success">{message}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label style={Texts.FormLabel}>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
 
-</Card.Title>
-        <Card.Body>
+              <Button disabled={loading} className="w-100" type="submit">
+                Reset Password
+              </Button>
 
-          <h4 className="text-center mb-4" style = {Texts.Heading}>Password Reset</h4>
+              {/* <p className="submitbtn" type="submit">Login</p> */}
+            </Form>
+          </Card.Body>
 
-<hr></hr>       
-     {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label style = {Texts.FormLabel}>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-
-     
-    <Button disabled={loading} className="w-100" type="submit">
-              Reset Password
-            </Button>
-         
-            {/* <p className="submitbtn" type="submit">Login</p> */}
-          </Form>
-
-        
-        </Card.Body>
-      
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div><br/>
-      </Card>
-      
+          <div className="w-100 text-center mt-2">
+            Need an account? <Link to="/signup">Sign Up</Link>
+          </div>
+          <br />
+        </Card>
       </Container>
-
-
-       </>
-  )
+    </>
+  );
 }
- 

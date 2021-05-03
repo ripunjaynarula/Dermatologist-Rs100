09214a09656d2d55991f195d0c56c62a7d846918
Shiv10 <shivanshsharma2012@gmail.com>
2021-05-03 21:04:24 +0530

    feat: yearwise sorting

--- Frontend/mediac/src/components/Details.js
+++ Frontend/mediac/src/components/Details.js
@@ -12,45 +12,65 @@ import { CardMain } from "../css/Card";
 import { CardBody, Col, Card, Container } from "reactstrap";
 import app from "../firebase";
 
-
 // var month_name = function(dt){
-  const mlist = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" ];
+const mlist = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 //     return mlist[dt.getMonth()];
 //   };
-var i,x,y,z;
+var i, x, y, z;
 
 function Details() {
-
   function callback(e) {
     console.log(e);
   }
 
   const { currentUser } = useAuth();
-  const [consultations, setConsultations] =  useState([]);
+  const [consultations, setConsultations] = useState([]);
+  const [consultationYears, setConsultationYears] = useState([]);
   const history = useHistory();
   useEffect(() => {
-    if(!currentUser){
-      history.push('/login')
+    if (!currentUser) {
+      history.push("/login");
     }
     //get data from backend
-    async function getConsultations(){
+    async function getConsultations() {
       const token = await app.auth().currentUser.getIdToken(true);
       const requestOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json", token: token },
-        body: JSON.stringify({email: currentUser.email})
+        body: JSON.stringify({ email: currentUser.email }),
       };
 
-      let res = await fetch('http://localhost:5000/getActiveConsultation', requestOptions);
+      let res = await fetch(
+        `${process.env.REACT_APP_API_URL}getActiveConsultation`,
+        requestOptions
+      );
       res = await res.text();
       res = JSON.parse(res);
       setConsultations(res["consultation"]);
-      console.log(res);
+      let years = [];
+      res["consultation"].forEach((consultation) => {
+        let year = consultation.startDate.substring(0, 4);
+        years.push(year);
+      });
+      setConsultationYears(years);
     }
     getConsultations();
     //get data from backend
   }, []);
-  
+
   return (
     <>
       <div className="home" style={{ display: "block" }}>
@@ -87,60 +107,98 @@ function Details() {
                       <Button className="submitbtn">Upload</Button>
                     </div>
                   </div>
-                  
                 </div>
-                <br/>
-                
+                <br />
+
                 {/* <p style={{color: "rgb(58, 57, 57)", fontWeight: "900", fontSize:"20px"}}>&nbsp;2021</p> */}
 
-                {consultations?<>
-                  {consultations.map((consultation) => (
+                {consultations ? (
+                  <>
+                    {consultationYears.map((year) => (
+                      <>
+                        <h4>{year}</h4>
+                        {consultations.map((consultation) =>
+                          consultation.startDate.substring(0, 4) == year ? (
+                            <>
+                              <div style={{ fontSize: "0px" }}>
+                                {
+                                  ((x =
+                                    parseInt(consultation.startDate[5]) * 10),
+                                  (y = parseInt(consultation.startDate[6])))
+                                }
+                              </div>
 
-                    <>
-                    <div style={{fontSize:"0px"}}>
-                    { x=parseInt(consultation.startDate[5])*10,
-                   y=parseInt(consultation.startDate[6])}</div>
-
-
-                   <div className="card" id="detailcard" style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
-                   <div class="card-body" id="detailcard" >
-                   <div className="float-left dmcal" style={{paddingBottom:"-5px"}}>
-                   
-                   
-
-                       <p className="datecal" style={{marginBottom:"-5px"}} ><b>{consultation.startDate[8]+consultation.startDate[9]}</b></p>
-                       <p className="monthcal" style={{marginBottom:"5px"}}>{mlist[(x+y)-1]}</p>
-                     </div>
-                     <div >
-                   
-                        <div className="consuldoc"  style={{display:"flex",marginleft:"50px"}}>
-                       <p ><b>Consulted Dr. {consultation.doctorName}</b><br/>
-                       Record for {consultation.name}</p>
-                       </div>
-                     </div>
-                     
-                   </div>
-                   
-                 </div>
-                 <br/>
-                 </>
-              ))}
-                </>:<>
-                <div
-                  class="card"
-                  id="detailcard"
-                  style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
-                >
-                  <div class="card-body" id="detailcard">
-                    <p>No Data Yet</p>
-                  </div>
-                  
-                </div>
-                <br/>
-                </>}
-                
-                
-                
+                              <div
+                                className="card"
+                                id="detailcard"
+                                style={{
+                                  backgroundColor: "rgba(255, 255, 255, 0.5)",
+                                }}
+                              >
+                                <div class="card-body" id="detailcard">
+                                  <div
+                                    className="float-left dmcal"
+                                    style={{ paddingBottom: "-5px" }}
+                                  >
+                                    <p
+                                      className="datecal"
+                                      style={{ marginBottom: "-5px" }}
+                                    >
+                                      <b>
+                                        {consultation.startDate[8] +
+                                          consultation.startDate[9]}
+                                      </b>
+                                    </p>
+                                    <p
+                                      className="monthcal"
+                                      style={{ marginBottom: "5px" }}
+                                    >
+                                      {mlist[x + y - 1]}
+                                    </p>
+                                  </div>
+                                  <div>
+                                    <div
+                                      className="consuldoc"
+                                      style={{
+                                        display: "flex",
+                                        marginleft: "50px",
+                                      }}
+                                    >
+                                      <p>
+                                        <b>
+                                          Consulted Dr.{" "}
+                                          {consultation.doctorName}
+                                        </b>
+                                        <br />
+                                        Record for {consultation.name}
+                                      </p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <br />
+                            </>
+                          ) : (
+                            <></>
+                          )
+                        )}
+                      </>
+                    ))}
+                  </>
+                ) : (
+                  <>
+                    <div
+                      class="card"
+                      id="detailcard"
+                      style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
+                    >
+                      <div class="card-body" id="detailcard">
+                        <p>No Data Yet</p>
+                      </div>
+                    </div>
+                    <br />
+                  </>
+                )}
               </TabPane>
               <TabPane tab="Medical Records" key="2">
                 <h2>Medical Records</h2>
@@ -165,9 +223,7 @@ function Details() {
               <TabPane tab="Need Help?" key="4">
                 <h2>Need Help?</h2>
                 <div class="card" id="detailcard">
-                  <div class="card-body" id="detailcard">
-                    
-                  </div>
+                  <div class="card-body" id="detailcard"></div>
                 </div>
               </TabPane>
             </Tabs>
